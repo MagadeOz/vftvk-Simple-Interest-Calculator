66ec4fae33debcdffee02be626ab8008e8f5f98b
MagadeOz <mposse@outlook.com>
2021-07-06 02:20:10 -0300

    MP: Simple interest calculator working ok

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  //Gets values
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+
+  //Calculates interest
+  var interest = principal * years * rate / 100;
+  //Calculates dates
+  var future_year = new Date().getFullYear() + parseInt(years);
+
+  //Real number validation
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+    return false;
+  } else {
+    //Shows the result
+    document.getElementById("result").innerHTML =
+      "If you deposit <span id='highlight'>" + principal + "</span>, <br/>" +
+      "at an interest rate of <span id='highlight'>" + rate + "</span>. <br/>" +
+      "You will receive an amount of <span id='highlight'>" + interest + "</span>, <br/>" +
+      "in the year <span id='highlight'>" + future_year + "</span>.";
+    return true;
+  }
 }
-        
+
+function rateValue() {
+  var updateVal = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerHTML = updateVal + "%";
+}
